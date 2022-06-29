6de5db7d3990f219449207ab32311e29000ec354
gokula krishnan <gokulakrishnan812@gmail.com>
2022-06-29 19:25:46 +0530

    Trigger rebuild

--- src/componats/Home.js
+++ src/componats/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <Section
         title='Model S'
         description='Order Online for Touchless Delivery'
-        backgroundImg='https://yt3.ggpht.com/URIi4aND7NsUFBx0aV996zu7wCgATCVokvqu9T3fFggRUO9Gc-yPpSAuupeScO9mDxXlE4jbsg=s88-c-k-c0x00ffffff-no-rj'
+        backgroundImg='model-3.jpg'
         leftBtnText='Custom order'
         rightBtnText='Exisiting inventory'
       />
